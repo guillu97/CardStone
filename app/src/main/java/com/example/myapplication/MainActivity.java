1ed217bdaab7ba910bc422229553cea4821f941a
Claire HUANG <claire.huang@efrei.net>
2019-03-19 23:12:42 +0100

    add the bottom navigation view ! and different fragment (pages)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -12,6 +15,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -50,7 +54,6 @@ public class MainActivity extends AppCompatActivity{
     private String url = "https://api.hearthstonejson.com/v1/25770/frFR/cards.collectible.json";
 
 
-
     public MainActivity() {
     }
 
@@ -58,7 +61,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mList = findViewById(R.id.main_list);
+        /*mList = findViewById(R.id.main_list);
 
         cardList = new ArrayList<>();
         adapter = new CardAdapter(getApplicationContext(),cardList);
@@ -76,9 +79,36 @@ public class MainActivity extends AppCompatActivity{
 
         requestQueue = Volley.newRequestQueue(this);
 
-        getData();
+        getData();*/
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView); // Barre de menu
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    // react to click on item
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_saved:
+                            selectedFragment = new SavedFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_create:
+                            selectedFragment = new CreateFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 
     private void getData() {
         final ProgressDialog progressDialog = new ProgressDialog(this);
